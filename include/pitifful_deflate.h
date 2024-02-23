1f3091444a66379f085d65fc7c77fc691a23c5de
Alec Heckert <alecheckert@gmail.com>
2024-02-23 12:54:51 -0500

    simpler DEFLATE invocation

--- include/pitifful_deflate.h
+++ include/pitifful_deflate.h
@@ -26,6 +26,27 @@
 
 namespace pitifful {
 
+/* interpret and report exit values of zlib ops */
+void zerr(int ret)
+{
+    switch(ret){
+        case Z_ERRNO:
+            std::cerr << "zlib error: stream handling error\n";
+            break;
+        case Z_STREAM_ERROR:
+            std::cerr << "zlib error: invalid compression level\n";
+            break;
+        case Z_DATA_ERROR:
+            std::cerr << "zlib error: invalid or incomplete deflate data\n";
+            break;
+        case Z_MEM_ERROR:
+            std::cerr << "zlib error: out of memory\n";
+            break;
+        case Z_VERSION_ERROR:
+            std::cerr << "zlib error: zlib version mismatch!\n";
+    }
+}
+
 class DEFLATEDecompressor{
     unsigned char inbuffer[CHUNK];
     unsigned char outbuffer[CHUNK];
@@ -47,65 +68,49 @@ public:
             throw std::runtime_error("input stream not open");
         }
         char* inptr = reinterpret_cast<char*>(&inbuffer[0]);
+        source.read(inptr, to_read);
 
         /* allocate inflate state */
         strm.zalloc = Z_NULL;
         strm.zfree = Z_NULL;
         strm.opaque = Z_NULL;
         strm.avail_in = 0;
-        strm.next_in = Z_NULL;
+        strm.avail_in = static_cast<unsigned>(to_read);
+        strm.next_in = inbuffer;
+        strm.avail_out = max_out_buf_size;
+        strm.next_out = reinterpret_cast<unsigned char*>(out);
+
+        // Initialize stream
         ret = inflateInit(&strm);
         if(ret!=Z_OK){
+            std::cerr << "error with inflateInit: " << ret << "\n";
+            zerr(ret);
             return ret;
         }
 
-        /* decompress until deflate streams ends or eof */
-        do {
-            int chunk_size = CHUNK < to_read ? CHUNK : to_read;
-            source.read(inptr, chunk_size);
-            strm.avail_in = chunk_size;
-            if(!source){
-                (void)inflateEnd(&strm);
-                return Z_ERRNO;
+        // Decompress
+        ret = inflate(&strm, Z_NO_FLUSH);
+        if(ret!=Z_STREAM_END){
+            std::cerr << "error with inflate: " << ret << "\n";
+            if(ret==Z_OK){
+                std::cerr << "zlib: stream okay, but incomplete decompression\n";
+            } else{
+                zerr(ret);
             }
-            if(strm.avail_in==0){
-                break;
-            }
-            strm.next_in = inbuffer;
-            /* run inflate() on input until output buffer not full */
-            do {
-                strm.avail_out = CHUNK;
-                strm.next_out = outbuffer;
-                ret = inflate(&strm, Z_NO_FLUSH);
-                assert(ret!=Z_STREAM_ERROR);
-                switch(ret){
-                    case Z_NEED_DICT:
-                        ret = Z_DATA_ERROR;
-                    case Z_DATA_ERROR:
-                    case Z_MEM_ERROR:
-                        (void)inflateEnd(&strm);
-                        return ret;
-                }
-                have = CHUNK - strm.avail_out;
-                if(max_out_buf_size<written+have){
-                    throw std::runtime_error(
-                        std::string("cannot write additional ")
-                        + std::to_string(have)
-                        + std::string(" bytes to output buffer with size ")
-                        + std::to_string(max_out_buf_size)
-                        + std::string(" bytes, only ")
-                        + std::to_string(max_out_buf_size-written)
-                        + std::string(" bytes remaining")
-                    );
-                }
-                std::memcpy(out, outbuffer, have);
-                out += have;
-                written += have;
-            } while (strm.avail_out == 0);
-            to_read -= chunk_size;
-        } while (ret != Z_STREAM_END);
-        (void)inflateEnd(&strm);
-        return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+            return ret;
+        }
+
+        // Clean up
+        ret = inflateEnd(&strm);
+        if(ret!=Z_OK){
+            std::cerr << "error with inflateEnd: " << ret << "\n";
+            zerr(ret);
+            return ret;
+        }
+
+        // Get number of bytes written
+        written = static_cast<unsigned>(strm.total_out);
+        return ret;
     }
 };
 
